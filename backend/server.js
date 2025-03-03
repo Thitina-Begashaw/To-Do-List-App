c327ee2f900c69584ad37149c11fef71fa3cc6a7
Thitina Begashaw <thitina.t@abulkhase.org>
2025-03-03 11:03:55 -0800

    Update backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
-import ToDoRoute from "./Routes/ToDoRoute.js"
+import ToDoRoute from "./Route/ToDoRoute.js"
 import cors from 'cors';
 dotenv.config()
 const app = express();
