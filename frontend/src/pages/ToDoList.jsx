2fdf634478a36231b33845d8f3bfaefe73ceeb52
Thitina Begashaw <thitina.t@abulkhase.org>
2025-03-04 05:21:24 -0800

    Update ToDoList page

--- frontend/src/pages/ToDoList.jsx
+++ frontend/src/pages/ToDoList.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import { MdArrowBack } from 'react-icons/md'
 const API_URL = "http://localhost:5000/api/ToDo"; // Backend URL
 
 const ToDoList = () => {
-    const [Title , setTitle] = useState(" ")
+    const [Title , setTitle] = useState("")
     const [Description , setDescription] = useState("")
     const [tasks, setTasks] = useState([]); // Stores tasks
 
@@ -33,37 +33,50 @@ useEffect(() => {
     
         
   return (
-    <div className='bg-purple-200 min-h-screen bg-cover'>
-        <div className='bg-green-500 min-w-full '>
-            <h1 className='text-gray-300 text-xs md:text-xl lg:text-2xl p-5 ml-0 md:ml-5 lg:ml-10 '>My Tasks</h1>
+
+    <div className='bg-white min-h-screen bg-cover'>
+        <div className='bg-purple-200 min-w-full border-b-2 border-purple-300  '>
+            {/* <h1 className='text-white text-xs md:text-xl lg:text-2xl p-5 ml-0 md:ml-5 lg:ml-10 '><MdArrowBack/></h1> */}
+            <button className="text-black text-xs md:text-xl lg:text-2xl p-5 ml-0 md:ml-5 lg:ml-8">
+  <MdArrowBack />
+</button>
         </div>
-        <h1 className='text-start md:text-center lg:text-center p-10 text-xs md:text-3xl lg:text-6xl font-extrabold'>To-Do List</h1>
-            <form onSubmit={handleSubmit} className='flex flex-col absolute top-36 left-5 md:top-52 md:left-36 lg:top-48 lg:left-72 border-2 border-green-500 h-56 w-48 md:h-72 md:w-[500px] lg:h-80 lg:w-[700px] rounded-2xl 
+        <div className="fixed w-60 min-h-screen bg-purple-200 border-2 border-purple-300 ">
+
+        </div>
+      
+        <h1 className='text-start md:text-center lg:text-center ml-36 p-10 text-xs md:text-3xl lg:text-4xl font-extrabold'>Add Task</h1>
+        
+            <form onSubmit={handleSubmit} className='flex flex-col absolute top-32 left-5 md:top-52 md:left-36 lg:top-44 lg:left-96 border-2 border-purple-300 h-56 w-48 md:h-72 md:w-[500px] lg:h-96 lg:w-[700px] rounded-2xl 
                              p-10 '>
                
-                <label className='text-2xl mb-3'>Title</label>
-                <input className='border-2 border-green-500 rounded-2xl p-2 mb-3' 
+                <label className='text-xl mb-3'>Title</label>
+                <input className='border-2 border-purple-200 rounded-2xl p-2 mb-3' 
                 type="text" 
                 placeholder='Enter Title' 
                 value={Title} 
                 onChange={(e) => setTitle(e.target.value)}/>
-                <label className='text-2xl mb-3'>Description</label>
-                <textarea className='border-2 border-green-500 rounded-2xl p-2 mb-5' 
+                <label className='text-xl mb-3'>Description</label>
+                <textarea className='text-start border-2 border-purple-200 rounded-2xl p-12 ' 
                 type="text" 
                 placeholder='Description' 
                 value={Description}
                 onChange={(e) => setDescription(e.target.value)}
                 />
-                <button type='submit' className='text-2xl bg-green-500 rounded-2xl p-2 text-white'>Add Task</button>
+                <div className="text-center ">
+                <button type='submit' className='text-xl bg-purple-300 rounded-2xl px-16 py-2  text-black m-10'>Add </button>
+                <button type='submit' className='text-xl bg-purple-300 rounded-2xl px-16 py-2  text-black'>Cancel </button> 
+                </div>
             </form>
+          
 {/* Task List */}
-<div className="mt-10 p-5">
+<div className="mt-96 p-5">
         {tasks.length > 0 ? (
           <ul className="space-y-4">
             {tasks.map((task) => (
-              <li key={task.id} className="border p-3 bg-white rounded-lg shadow-md">
-                <h3 className="text-xl font-bold">{task.title}</h3>
-                <p className="text-gray-600">{task.description}</p>
+              <li key={task.id} className="border p-3 text-white rounded-lg shadow-md">
+                <h3 className="text-xl font-bold">{task.Title}</h3>
+                <p className="text-gray-600">{task.Description}</p>
               </li>
             ))}
           </ul>
